84cfe04c47a067a3154ba6b3aa0e970c7f72d27b
alireza-haeri <alireza.haeri.dev@gmail.com>
2025-08-23 19:26:53 +0330

    feat: add advance search endpoint
    -implement multiple filters for detailed search
    -implement suggestion when few result
    -implement sorting
    
    Refs #1

---
+++ Projects/Searchify.Api/Common/Helper/ElasticSerachHelper.cs
@@ -0,0 +1,19 @@
+﻿using Elastic.Clients.Elasticsearch;
+using Elastic.Clients.Elasticsearch.QueryDsl;
+
+namespace Searchify.Api.Common.Helper;
+
+public static class ElasticSearchHelper
+{
+    public static void AddMatchIfNotEmpty(this List<Query> queries, Field field, string? value)
+    {
+        if (!string.IsNullOrWhiteSpace(value))
+            queries.Add(new MatchQuery { Field = field, Query = value });
+    }
+
+    public static void AddTermIfNotEmpty(this List<Query> queries, Field field, string? value)
+    {
+        if (!string.IsNullOrWhiteSpace(value))
+            queries.Add(new TermQuery { Field = field, Value = value });
+    }
+}
