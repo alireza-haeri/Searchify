0f971e3998e00573f6e817cbfa344ddc89ca25e9
alireza-haeri <alireza.haeri.dev@gmail.com>
2025-08-24 10:25:17 +0330

    refactor: get book by isbn endpoint fix: advance search endpoint
    Refs #8

--- Projects/Searchify.Api/Endpoints/Book/Search/GetBookByIsbnEndpoint.cs
+++ Projects/Searchify.Api/Endpoints/Book/Search/GetBookByIsbnEndpoint.cs
@@ -11,7 +11,7 @@ public class GetBookByIsbnEndpoint : BookEndpointBase
     {
         var group = MapBookGroup(app);
 
-        group.MapGet("{isbn}", async (
+        group.MapGet("{isbn:length(10,13)}", async (
             ElasticsearchClient client,
             CancellationToken token,
             [FromRoute] string? isbn) =>
